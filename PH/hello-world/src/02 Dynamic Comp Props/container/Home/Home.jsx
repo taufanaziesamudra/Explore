4388a9a07c45be92beb47727e56ed072b226c655
Taufan AS <taufanaziesamudra@gmail.com>
2021-10-23 01:26:08 +0700

    belajar lagi

---
+++ PH/hello-world/src/02 Dynamic Comp Props/container/Home/Home.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import YouTubeComp from '../../component/YouTubeComp/YouTubeComp'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <p>YouTube Component</p>
+                <hr />
+                {/* Membuat props dengan nama time, title dan desc
+                dimana props tersebut akan dikirimkan ke YouTubeComp */}
+                <YouTubeComp
+                    time="00:01"
+                    title="Aku belajar"
+                    desc="Belajar Ngoding" />
+                <YouTubeComp
+                    time="00:02"
+                    title="Aku sedang belajar"
+                    desc="Belajar JavaScript" />
+                <YouTubeComp
+                    time="00:03"
+                    title="Kadang aku males"
+                    desc="Belajar React JS" />
+                <YouTubeComp
+                    time="00:04"
+                    title="aku sedang belajar lagi"
+                    desc="Bealajr fullstack" />
+                <YouTubeComp />
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ PH/hello-world/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './02 Dynamic Comp Props/container/Home/Home';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import HelloComponent from './01 StateLess & StateFull Comp/component/HelloComponent';
+
+// const HelloWorld = () => {
+//   return <p>Hello Wordl</p>
+// }
+
+// Functional Component atau StateLess Component
+// const HelloComponent = () => {
+//   return <P>Hello Component</P>
+// }
+
+
+// Class Component atau StateFull Component
+// class StateFullCompnent extends React.Component {
+//   render() {
+//     return <p>StateFullCompnent</p>
+//   }
+// }
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
