2eb294a3a204fa80081beb8056d2137f4e25edef
Taufan AS <taufanaziesamudra@gmail.com>
2021-11-05 23:31:29 +0700

    push

---
+++ PH/simple-notes-firebase/src/01 Integrasi Firebase React JS/config/firebase/index.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDcFY-93Pk2y8jKuKCHLkYmRba0Jqp5X_k",
+    authDomain: "simple-notes-firebase-bbd22.firebaseapp.com",
+    projectId: "simple-notes-firebase-bbd22",
+    storageBucket: "simple-notes-firebase-bbd22.appspot.com",
+    messagingSenderId: "420089331300",
+    appId: "1:420089331300:web:0b9ffd478e58512575e971",
+    measurementId: "G-9VT1QS8YJX"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
