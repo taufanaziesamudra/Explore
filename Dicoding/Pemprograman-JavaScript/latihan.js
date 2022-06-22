0bbea053560c82e51a4e4cfb9fb1280ab86911bd
Taufan AS <taufanaziesamudra@gmail.com>
2022-06-22 19:07:12 +0700

    push rank

---
+++ Dicoding/Pemprograman-JavaScript/latihan.js
@@ -0,0 +1,83 @@
+// !!! Tipe Data
+
+// TODO Undefined
+// let x;
+// console.log(typeof (x));
+
+// TODO Numbers
+// let x = 10;
+// console.log(typeof (x))
+// let y = 17.25;
+// console.log(typeof (y))
+// --- //
+// let a = 12;
+// let b = 9;
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// --- //
+// let postfix = 5;
+// console.log(postfix++);
+// console.log(postfix);
+// let prefix = 5;
+// console.log(++prefix);
+
+// TODO BigInt
+// const bigNumber = 1234567890123456789012345678901234567890n;
+// const myInt = 1234567890123456789012345678901234567890;
+// console.log(bigNumber);
+// console.log(myInt);
+// --- //
+// const bigIntButSmall = 7n;
+// console.log(5n + 2n);
+// console.log(5n - 2n);
+// console.log(5n * 2n);
+// console.log(5n / 2n);
+// console.log(5n % 2n);
+
+// TODO Strings
+// let greet = "Hello";
+// console.log(typeof (greet))
+// --- //
+// const question = '"What do you think of JavaScript?" I asked';
+// console.log(question)
+// --- //
+// const answer = '"I think it\'s awesome!" he answered confidently';
+// console.log(answer)
+// --- //
+// let greeet = "Hello";
+// let moreGreet = greeet + greeet;
+// console.log(moreGreet);
+
+// TODO Boolean
+// let x = true;
+// let y = false;
+// console.log(typeof (x))
+// console.log(typeof (y))
+// --- //
+// const a = 10;
+// const b = 12;
+// let isGreater = a > b;
+// let isLess = a < b;
+// console.log(isGreater);
+// console.log(isLess);
+
+// TODO Null
+// let someLaterData = null;
+// console.log(someLaterData);
+
+// TODO Null
+// const id = Symbol("id");
+// console.log(id);
+// --- //
+// const id1 = Symbol("id");
+// const id2 = Symbol("id");
+// console.log(id1 == id2);
+
+const sayHeloo = () => {
+  console.log("Selamat pagi")
+}
+
+sayHeloo()
